1c6ff8d5545046f3701306eaa624ce34a599318f
IlGrandeLuca <lucailgrande96@outlook.it>
2022-01-17 15:47:15 +0100

    Update student.js

--- student.js
+++ student.js
@@ -1,4 +1,4 @@
-const student = {
+/*const student = {
     firstName: ['Luca'],
     lastName: ['Il Grande'],
     age: [25]
@@ -6,4 +6,40 @@ const student = {
 
 for (let key in student) {
     console.log(student[key]);
+};*/
+
+let students = [
+    {
+        firstName: ['Savio'],
+        lastName: ['D`Ambrosio'],
+        age: [24]
+    },
+    {
+        firstName: ['Gimmy'],
+        lastName: ['Bosetti'],
+        age: [25]
+    },
+    {
+        firstName: ['Roberta'],
+        lastName: ['Angioni'],
+        age: [29]
+    }
+];
+
+let studentName = prompt('Enter name');
+let studentLastName = prompt('Enter lastname');
+let studentAge = prompt('Enter age');
+
+students.push({
+    firstName: studentName,
+    lastName: studentLastName,
+    age: studentAge,
+});
+
+for (let i = 0; i < students.length; i++) {
+    let currentStudent = students[i];
+    console.log('Name[' + i + ']: ' + currentStudent.firstName);
+    console.log('Lastame[' + i + ']: ' + currentStudent.lastName);
+    console.log('Age[' + i + ']: ' + currentStudent.age);
 }
+console.log(students);
